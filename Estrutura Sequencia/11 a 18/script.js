f063cff234bfdb02bc03c148dcd66954728105cf
AlanDiogoR <alandiogor@gmail.com>
2023-01-01 22:11:08 -0300

    execicios sequenciais terminados

---
+++ Estrutura Sequencia/11 a 18/script.js
@@ -0,0 +1,33 @@
+function twoNumbersInt() {
+  let n1 = Number(prompt('Infomr o primeiro número: '));
+  let n2 = Number(prompt('Infomr o segundo número: '));
+  alert(`O produto do dobro do primeiro: ${(n1 * 2) * (n1 * 2)} com metade do segundo: ${n2 / 2} = ${((n1 * 2) * (n1 * 2)) + (n2 / 2)} `);
+}
+
+function heightPeople() {
+  alert('oi');
+}
+
+function SexoPeople() {
+  alert('oi');
+}
+
+function fishermanTalk() {
+  alert('oi');
+}
+
+function earningPerHour() {
+  alert('oi');
+}
+
+function paintStoreTree() {
+  alert('oi');
+}
+
+function paintStoreSix() {
+  alert('oi');
+}
+
+function calculateSpeed() {
+  alert('oi');
+}
